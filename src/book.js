dd01181e521dcd841f207c592c38477f08d10115
Naomi Ware <nware1066@yahoo.com>
2020-05-06 21:15:43 -0600

    create title and build main character

--- src/book.js
+++ src/book.js
@@ -1,10 +1,14 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {name, age, pronouns};
 }
 
 module.exports = {
   createTitle: createTitle,
-  // buildMainCharacter: buildMainCharacter,
+  buildMainCharacter: buildMainCharacter,
   // saveReview: saveReview,
   // calculatePageCount: calculatePageCount,
   // writeBook: writeBook,
