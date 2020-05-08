8f117f6820ab535fa71e64126a25c8e8cd4d8862
Naomi Ware <nware1066@yahoo.com>
2020-05-08 16:58:31 -0600

    create ability to shelve and unshelve books

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,23 @@
+function shelfBook(book, scifiShelf) {
+  if (scifiShelf.length < 3) {
+    scifiShelf.unshift(book);
+  };
+}
 
+// iterate over titles in shelf array
+// see if title of book in array matches title of book being passed
+// if title matches, remove title
+
+function unshelfBook(title, shelf) {
+  var indexOfTarget = shelf.findIndex(function(book) {
+    return title === book.title;
+  });
+  shelf.splice(indexOfTarget, 1);
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
   // listTitles: listTitles,
   // searchShelf: searchShelf
 };
