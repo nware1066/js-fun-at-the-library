d7089aa4e7cd31f51cc98c3c9f3501d59db7e5ed
Naomi Ware <nware1066@yahoo.com>
2020-05-09 15:27:13 -0600

    create library function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,22 @@
 
+function createLibrary(name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  };
+}
+
+// takes in a string as a parameter
+// returns a string as a value of name key in a library object
+
+
 
 module.exports = {
-  // createLibrary: createLibrary,
+  createLibrary: createLibrary,
   // addBook: addBook,
   // checkoutBook: checkoutBook
 };
