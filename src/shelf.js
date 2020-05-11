84125a1f5ec473c5fa0159df5239f00f56fcc16e
Naomi Ware <nware1066@yahoo.com>
2020-05-10 19:35:02 -0600

    complete Librarian and remove comments in all files

--- src/shelf.js
+++ src/shelf.js
@@ -4,10 +4,6 @@ function shelfBook(book, scifiShelf) {
   };
 }
 
-// iterate over titles in shelf array
-// see if title of book in array matches title of book being passed
-// if title matches, remove title
-
 function unshelfBook(title, shelf) {
   var indexOfTarget = shelf.findIndex(function(book) {
     return title === book.title;
@@ -15,14 +11,9 @@ function unshelfBook(title, shelf) {
   shelf.splice(indexOfTarget, 1);
 }
 
-// loop through the input to find the title value of the book object
-// return a string of the title values
-// assign the string to a variable
-
 function listTitles(shelf) {
   return shelf.map(book => book.title).join(", ");
 }
-
 
 function searchShelf(shelf, title) {
   return shelf.some(book => book.title === title);
