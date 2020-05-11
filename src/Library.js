d555595e69b49d4552085264d32e6a14e1975353
Naomi Ware <nware1066@yahoo.com>
2020-05-10 19:33:21 -0600

    complete Librarian and remove comments from other files

--- src/Library.js
+++ src/Library.js
@@ -18,19 +18,38 @@ function checkoutBook(library, title) {
   var genres = Object.keys(library.shelves);
   for (var i = 0; i < genres.length; i++) {
     var shelf = library.shelves[genres[i]];
-    for (var j = 0; j < shelf.length; j++) {
-      if (shelf[j].title === title) {
-        shelf.splice(j, 1);
-        return `You have now checked out ${title} from the ${library.name}`;
-      }
+    if (checkShelf(shelf, title)) {
+      library.shelves[genres[i]] = removeBook(library, genres[i], title);
+      return giveFeedback(title, library, true);
     }
   }
-  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  return giveFeedback(title, library, false);
 }
+
+function checkShelf(shelf, title) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function removeBook(library, genre, title) {
+  return library.shelves[genre].filter(book => book.title !== title);
+}
+
+function giveFeedback(title, library, bool) {
+  var found = `You have now checked out ${title} from the ${library.name}`;
+  var notFound = `Sorry, there are currently no copies of ${title} available at the ${library.name}`;
+  return bool ? found : notFound;
+}
+
 
 
 module.exports = {
   createLibrary: createLibrary,
   addBook: addBook,
-  checkoutBook: checkoutBook
+  checkoutBook: checkoutBook,
+  checkShelf: checkShelf
 };
